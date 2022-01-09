d160c147f54da3b8d1bea0fedfe6231ef8af08e1
Kenneth Imade <kenneth.imade@yahoo.com>
2022-01-09 14:52:03 +0100

    Added function to access elements on chessboard

--- lib/board.rb
+++ lib/board.rb
@@ -8,4 +8,23 @@ class ChessBoard
         8.times{|i| board << i = Array.new(8)}
         board
     end
+
+    def create_board_keys
+        chess_cells = []
+        (0..7).each do |i|
+            (0..7).each do |j|
+                chess_cells << [j,i]
+            end
+        end
+
+        board_keys =  {}
+        index = 0
+        ("a".."h").each do |i|
+            ("1".."8").each do |j|
+                board_keys[i+j] = chess_cells[index]
+                index += 1
+            end
+        end
+        board_keys
+    end
 end
